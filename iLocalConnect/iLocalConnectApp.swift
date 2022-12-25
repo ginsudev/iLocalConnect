109ea73a36fb59b47130d985cc2d22830eb1f866
ginsudev <noahlittle199@gmail.com>
2022-12-25 19:20:05 +1100

    Initial Commit

---
+++ iLocalConnect/iLocalConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLocalConnectApp.swift
+//  iLocalConnect
+//
+//  Created by Noah Little on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iLocalConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
